a5b753a455412c275ec8453962d99b48ea5850ed
manianis <manianis@yahoo.fr>
2020-11-09 18:52:43 +0100

    basic files

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 18:47:11 2020
+
+@author: hp
+"""
+
+from distutils.core import setup
+setup(
+      name = 'tnalgo',
+      packages = ['tnalgo'],
+      version = 0.1,
+      description = 'Convention algorithme',
+      author = 'Mohamed Anis MANI',
+      author_email = 'manimohamed@gmail.com',
+      url = 'https://github.com/manimanis/tnalgo',
+      download_url = 'https://github.com/manimanis/tnalgo/tarball/0.1',
+      keywords = ['algorithme', 'tunisie'],
+      classifiers = [],
+)
